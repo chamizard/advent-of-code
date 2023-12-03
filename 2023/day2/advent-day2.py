b8d70657261425b57c12d803d214d1b065443409
Cameron Mann <63566269+chamizard@users.noreply.github.com>
2023-12-03 01:55:59 -0500

    Day 2 part two completed

--- 2023/day2/advent-day2.py
+++ 2023/day2/advent-day2.py
@@ -1,5 +1,5 @@
 # Part one solution
-# uncomment 
+# uncomment replace appropriate text for your input and run
 """
 # constants for readability
 RED = 0
@@ -44,6 +44,60 @@ sum = 0
 for line in input_file:
     result = check_game(line)
     print(result)
+    sum += result
+
+print("Sum: " + str(sum))
+input_file.close()
+"""
+# Part two solution below
+# uncomment replace appropriate text for your input and run
+"""
+# @arg to_parse: input line read from file
+# @return: power = minimum allowed values of each color per line/game
+def game_power(to_parse):
+    to_parse = to_parse.strip()
+    first_parse = to_parse.lstrip('Game ')
+    # print("first parse: " + first_parse)
+    id = first_parse.split(':')[0]
+    # print(id)
+    second_parse = first_parse.lstrip('1234567890:')
+    # print("second_parse: " + second_parse)
+    bags = second_parse.split(';')
+    if bags[1] == '':
+        print('Single value')
+    # print("og line: " + to_parse)
+    min_red = 1
+    min_green = 1
+    min_blue = 1
+    for bag in bags:
+        # print("bag: " + bag)
+        bag_items = bag.split(',')
+        for item in bag_items:
+            # print("item: " + item)
+            if 'red' in item:
+                val = item.removesuffix(' red')
+                # print("red val: " + val)
+                if int(val) > min_red:
+                    min_red = int(val)
+            if 'green' in item:
+                val = item.removesuffix('green')
+                # print(val)
+                if int(val) > min_green:
+                    min_green = int(val)
+            if 'blue' in item:
+                val = item.removesuffix(' blue')
+                if int(val) > min_blue:
+                    min_blue = int(val)
+    return min_red * min_green * min_blue
+
+# replace this with the name of your file
+# file must be in same directory
+fname = 'day2-input.txt'
+input_file = open(fname, 'r')
+sum = 0
+for line in input_file:
+    result = game_power(line)
+    # print(result)
     sum += result
 
 print("Sum: " + str(sum))
